9d09caadc2a921a1ba7d0ae1e5f54067aca1a1b9
TheScarletArrow <anton20001701@yandex.ru>
2020-05-13 17:13:28 +0300

    v0.1.2
    Signed-off-by: TheScarletArrow <anton20001701@yandex.ru>

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #define max(a,b) a>b?a:b
 #define min(a,b) a<b?a:b
-
+int intlength(int);
 int CharToBin(char ch){
 int val;
     for (int i=7;i>=0;i--)
@@ -19,6 +19,19 @@ int main() {
     char inputString[1024];
     char outputStringEncrypted[1024];
     char mask[1024];
+puts("Do you want to send message or either receive it?\n To send press 1, to receive press 2");
+int choice;
+////switch (choice) {
+//    case 1:
+//        puts("1");
+//        break;
+//    case 2:
+//        puts("2");
+//        break;
+//    default:
+//        puts("Wrong number");
+//        break;
+//}
 
 
     puts("Enter your message");
@@ -30,5 +43,23 @@ int first;
 for (int i=0;i<strlen(inputString);i++)
    first= CharToBin(inputString[i]);
 printf("%l", first);
+
+//#TODO need to make binary int to char
+for(int i=0; i<intlength(first); i+=8){
+    outputStringEncrypted[i] = (char)first;
+}
+
+puts(outputStringEncrypted);
 return 0;
+}
+
+int intlength(int number)
+{
+    int length=0;
+
+    while(number) {
+        length++;
+        number/=10;
+    }
+    return length;
 }
